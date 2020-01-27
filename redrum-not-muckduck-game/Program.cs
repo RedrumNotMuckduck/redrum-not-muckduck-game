1d46c616beb074ccd3070c8114ee03aa040f5791
Melissa Stock <melissastock44@gmail.com>
2020-01-27 09:25:55 -0800

    first commit

---
+++ redrum-not-muckduck-game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace redrum_not_muckduck_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
