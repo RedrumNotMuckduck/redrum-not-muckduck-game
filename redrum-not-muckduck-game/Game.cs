94e62db2209cf0b81402e2415b81f0a30304b34e
Wei Kuang <50929180+kuangwei8@users.noreply.github.com>
2020-01-28 11:00:46 -0800

    Done refactoring game.cs

--- redrum-not-muckduck-game/Game.cs
+++ redrum-not-muckduck-game/Game.cs
@@ -6,83 +6,80 @@ namespace redrum_not_muckduck_game
 {
     class Game
     {
-        public Room accounting { get; set; }
-        public Room sales { get; set; }
-        public Room kitchen { get; set; }
-        public Room breakroom { get; set; }
-        public Room reception { get; set; }
-        public Room annex { get; set; }
-        public static Room currentRoom { get; set; }
+        public Room Accounting { get; set; }
+        public Room Sales { get; set; }
+        public Room Kitchen { get; set; }
+        public Room Breakroom { get; set; }
+        public Room Reception { get; set; }
+        public Room Annex { get; set; }
+        public static Room CurrentRoom { get; set; }
+        public Board Board = new Board(); 
         public static int NUMBER_OF_LIVES { get; set; } = 3;
+        private bool IsGameOver = false; 
 
         public Game()
         {
-            accounting = new Room(
+            Accounting = new Room(
                "Accounting",
                "Angela's cat, Bandit",
                "Oscar: I am going into the ceiling"
                );
-            currentRoom = accounting;
-            sales = new Room(
+            CurrentRoom = Accounting;
+            Sales = new Room(
                "Sales",
                "a random torch",
                "Andy: This would never happen at Cornell"
                );
 
-            kitchen = new Room(
+            Kitchen = new Room(
                 "Kitchen",
                 "Oscar falling out of ceiling",
                 "Phyllis: I saw Dwight came from the break room"
                 );
 
-            breakroom = new Room(
+            Breakroom = new Room(
                 "Breakroom",
                 "nothing but you see the vending machine.",
                 "No one is here"
                 );
 
-            reception = new Room(
+            Reception = new Room(
                 "Reception",
                 "nothing",
                 "Pam: the door is locked"
                 );
-            annex = new Room(
+            Annex = new Room(
                 "The Annex",
                 "beet stained cigs, warning label 'could cause fire'",
                 "Kelly: Why does Dwight have a blow horn?"
                 );
 
-            accounting.AdjacentRoom = new List<Room> { sales };
-            sales.AdjacentRoom = new List<Room> { reception, accounting, kitchen };
-            reception.AdjacentRoom = new List<Room> { sales };
-            kitchen.AdjacentRoom = new List<Room> { sales, annex };
-            annex.AdjacentRoom = new List<Room> { kitchen, breakroom };
-            breakroom.AdjacentRoom = new List<Room> { annex };
+            Accounting.AdjacentRoom = new List<Room> { Sales };
+            Sales.AdjacentRoom = new List<Room> { Reception, Accounting, Kitchen };
+            Reception.AdjacentRoom = new List<Room> { Sales };
+            Kitchen.AdjacentRoom = new List<Room> { Sales, Annex };
+            Annex.AdjacentRoom = new List<Room> { Kitchen, Breakroom };
+            Breakroom.AdjacentRoom = new List<Room> { Annex };
         }
 
         public void PlayGame()
         {
-            Board current = new Board();
-            current.AddItemToFoundItems();
-            current.UpdateCurrentPlayerLocation("Melissa");
-            current.Render();
+            Board.AddItemToFoundItems();
+            Board.UpdateCurrentPlayerLocation("Melissa");
+            Board.Render();
 
             Console.WriteLine("Welcome to the Office");
 
-            while (!EndGame())
+            while (!IsGameOver)
             {
                 UserTurn();
             }
         }
 
-        private bool EndGame()
-        {
-            return false;
-        }
-
         private void UserTurn()
         {
             string userChoice = "";
+            Console.WriteLine("You can explore, talk to someone, leave the current room, or quit playing");
             Console.WriteLine("> ");
             userChoice = Console.ReadLine().ToLower();
             Console.WriteLine();
@@ -93,41 +90,40 @@ namespace redrum_not_muckduck_game
                     LeaveTheRoom();
                     break;
                 case "explore":
-                    Console.WriteLine(currentRoom.ItemInRoom);
+                    Console.WriteLine($"You found: {CurrentRoom.ItemInRoom}");
                     break;
                 case "talk":
-                    Console.WriteLine(currentRoom.PersonInRoom);
+                    Console.WriteLine(CurrentRoom.PersonInRoom);
                     break;
-                case "q":
-                    EndGame();
+                case "quit":
+                    IsGameOver = !IsGameOver; 
                     Console.WriteLine("\nThanks for playing. Goodbye. ");
                     break;
                 default:
-                    Console.WriteLine("Please enter a valid option: (explore, talk, leave, q)");
+                    Console.WriteLine("Please enter a valid option: (explore, talk, leave, quit)");
                     break;
             }
         }
 
         private void LeaveTheRoom()
         {
-            string nextRoom = "";
             Console.Write("You have the choice to go to: ");
-            for (int i = 0; i < currentRoom.AdjacentRoom.Count; i++)
+            for (int i = 0; i < CurrentRoom.AdjacentRoom.Count; i++)
             {
-                Console.Write($"{currentRoom.AdjacentRoom[i].RoomName} ");
+                Console.Write($"{CurrentRoom.AdjacentRoom[i].RoomName} ");
             }
-
-            nextRoom = Console.ReadLine().ToLower();
+            // TODO: error handling for user input 
+            string nextRoom = Console.ReadLine().ToLower();
             UpdateCurrentRoom(nextRoom);
         }
 
         private void UpdateCurrentRoom(string nextRoom) 
         {
-            for (int i = 0; i < currentRoom.AdjacentRoom.Count; i++)
+            for (int i = 0; i < CurrentRoom.AdjacentRoom.Count; i++)
             {
-                if (nextRoom == currentRoom.AdjacentRoom[i].RoomName.ToLower())
+                if (nextRoom == CurrentRoom.AdjacentRoom[i].RoomName.ToLower())
                 {
-                    currentRoom = currentRoom.AdjacentRoom[i];
+                    CurrentRoom = CurrentRoom.AdjacentRoom[i];
                 }
             }
         }
